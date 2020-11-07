1d005d619c462e8db415b87be7f133d2673046ff
stef2georg <stefan.georgiev91@gmail.com>
2020-11-07 14:26:57 +0200

    Lecture 6

---
+++ src/lecture6/Time.java
@@ -0,0 +1,18 @@
+package lecture6;
+
+public class Time {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        int sum = 0;
+        for (int i = 0; i < 100000; i++) {
+            sum += i;
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Time: " + (endTime - startTime));
+
+        Character.isLetterOrDigit('a');
+    }
+}
